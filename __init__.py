580e7cc1766c5333962e7daff9f0e1c885c88e59
Matias Fernandez <matiaspersonalxzh@gmail.com>
2026-01-04 22:38:19 -0300

    Covarianza

---
+++ __init__.py
@@ -0,0 +1,27 @@
+import math
+
+def desviacion_estandar(datos):
+    """
+    Calcula la desviación estándar poblacional de una lista de valores
+    numéricos, ignorando valores NaN.
+
+    La desviación estándar corresponde a la raíz cuadrada de la varianza
+    y se expresa en las mismas unidades que los datos originales.
+
+    Parámetros
+    ----------
+    datos : list
+        Lista de valores numéricos (int o float). Los valores NaN
+        son ignorados en el cálculo.
+
+    Retorna
+    -------
+    float
+        Desviación estándar de los valores válidos.
+
+    Lanza
+    -----
+    Las excepciones son propagadas desde la función varianza.
+    """
+    
+    return math.sqrt(varianza(datos))
