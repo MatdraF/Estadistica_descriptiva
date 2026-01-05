77adb22c07f4cf3a8b3871655b7abbf4ba65d39b
Matias Fernandez <matiaspersonalxzh@gmail.com>
2026-01-04 23:53:10 -0300

    Arreglos

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ from .mediana import mediana
 from .moda import moda
 from .percentil import percentil
 from .rango import rango
-from .iqr import rango_intercuartilico
+from .rango_intercuartilico import rango_intercuartilico
 from .varianza import varianza
 from .desviacion_estandar import desviacion_estandar
-from .mad import desviacion_mediana_absoluta
+from .desviacion_mediana_absoluta import desviacion_mediana_absoluta
 from .covarianza import covarianza
